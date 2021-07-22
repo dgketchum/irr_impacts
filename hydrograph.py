330911a5871e80440720671b46d185f191daae03
dgketchum <pdWs7kyy>
2021-07-22 13:18:51 -0600

    move hydrograph() to it's own module

---
+++ hydrograph.py
@@ -0,0 +1,22 @@
+import os
+
+from pandas import read_csv, to_datetime
+
+
+def hydrograph(c):
+    df = read_csv(c)
+    try:
+        df['dt'] = to_datetime(df['dt'])
+    except KeyError:
+        df['dt'] = to_datetime(df['datetimeUTC'])
+    df = df.set_index('dt')
+    try:
+        df.drop(columns='datetimeUTC', inplace=True)
+    except KeyError:
+        pass
+    return df
+
+
+if __name__ == '__main__':
+    pass
+# ========================= EOF ====================================================================
