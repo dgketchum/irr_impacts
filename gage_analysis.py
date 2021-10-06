bb79866f3c57523e7b594dd4a83000face5b416c
dgketchum <pdWs7kyy>
2021-10-05 20:15:13 -0600

    improve baseflow correlation search

--- gage_analysis.py
+++ gage_analysis.py
@@ -6,10 +6,11 @@ from pprint import pprint
 
 import numpy as np
 from scipy.stats.stats import linregress
-from pandas import DataFrame, read_csv
+from pandas import DataFrame, read_csv, concat
 from datetime import date
 from dateutil.relativedelta import relativedelta as rdlt
 import fiona
+import statsmodels.api as sm
 
 from hydrograph import hydrograph
 
@@ -386,24 +387,76 @@ CLMB_STATIONS = ['12302055',
                  '14243000']
 
 
+def daily_median_flow(daily_q_dir, metadata_in):
+    l = [os.path.join(daily_q_dir, x) for x in os.listdir(daily_q_dir)]
+    l.sort()
+    d = {}
+    with open(metadata_in, 'r') as f:
+        meta = json.load(f)
+    for c in l:
+        try:
+            sid = os.path.basename(c).split('.')[0]
+            d[sid] = {}
+            if sid not in meta.keys():
+                continue
+            df = hydrograph(c)
+            s, e = df.index[0], df.index[-1]
+            annual_df = None
+            first = True
+            for y in range(s.year, e.year + 1):
+                ydf = copy(df['q'].loc['{}-01-01'.format(y): '{}-12-31'.format(y)])
+                if ydf.shape[0] < 365:
+                    continue
+                ydf.name = 'q_{}'.format(y)
+                months = [i.month for i, r in ydf.iteritems()]
+                days = [i.day for i, r in ydf.iteritems()]
+                if first:
+                    base_idx = ['{}-{}'.format(m, d) for m, d in zip(months, days)]
+                    ydf.index = base_idx
+                    annual_df = ydf
+                    first = False
+                else:
+                    idx = ['{}-{}'.format(m, d) for m, d in zip(months, days)]
+                    ydf.index = idx
+                    ydf = ydf.reindex(base_idx)
+                    if np.count_nonzero(np.isnan(ydf.values)) > 0:
+                        continue
+                    else:
+                        annual_df = concat([annual_df, ydf], axis=1)
+        except Exception as e:
+            print('error', sid, e)
+
+        try:
+            medians = annual_df.median(axis=1)
+            d[sid]['max_mo'] = months[medians.values.argmax()]
+            d[sid]['min_mo'] = months[medians.values.argmin()]
+            d[sid]['max_median_q'] = medians.max()
+            d[sid]['min_median_q'] = medians.min()
+        except Exception as e:
+            pass
+
+    return d
+
+
 def low_flow_months(monthly_q_dir, n_months=1):
     l = [os.path.join(monthly_q_dir, x) for x in os.listdir(monthly_q_dir)]
     d = {}
     for c in l:
         sid = os.path.basename(c).split('.')[0]
         d[sid] = {}
-        full_yr_rec = 0
+        full_yr_rec = []
         df = hydrograph(c)
         if 'qb' not in list(df.columns):
             continue
         s, e = df.index[0], df.index[-1]
         lf_months = []
+
         for y in range(s.year, e.year + 1):
             ydf = copy(df.loc['{}-01-01'.format(y): '{}-12-31'.format(y)])
             if np.count_nonzero(np.isnan(ydf['qb'].values)) > 0:
                 continue
             else:
-                full_yr_rec += 1
+                full_yr_rec.append(y)
             series = ydf['qb'].values
             idx = np.argpartition(series, n_months) + 1
             lf_months += list(idx[:m])
@@ -424,15 +477,19 @@ def irrigation_metadata(csv):
     for s, r in df.iterrows():
         d[s] = {}
         i = np.array([r[x] for x in r.index if 'irr' in x])
-        d[s]['mean_'], d[s]['std'], d[s]['norm_std'] = i.mean(), i.std(), i.std() / i.mean()
+        if i.mean() > 1e-4:
+            d[s]['mean_'], d[s]['std'], d[s]['norm_std'] = i.mean(), i.std(), i.std() / i.mean()
+        else:
+            d[s]['mean_'], d[s]['std'], d[s]['norm_std'] = 0, 0, 0
     return d
 
 
-def get_basin_meatadata(base_metadata, monthly_q_dir, irr_csv, metadata_out, months=3):
+def get_basin_metadata(base_metadata, monthly_q_dir, daily_q_dir, irr_csv, metadata_out, months=3):
     with open(base_metadata, 'r') as f:
         metadata = json.load(f)
-    irr = irrigation_metadata(irr_csv)
+    dq = daily_median_flow(daily_q_dir, base_metadata)
     lf = low_flow_months(monthly_q_dir, n_months=months)
+    irr = irrigation_metadata(irr_csv)
     new_metadata = {}
     for sid, v in metadata.items():
         try:
@@ -441,13 +498,25 @@ def get_basin_meatadata(base_metadata, monthly_q_dir, irr_csv, metadata_out, mon
             norm_std = irr[sid]['norm_std'] / (metadata[sid]['AREA_SQKM'] * 1e6)
             lowflo = lf[sid]['lf_m']
             qb_yrs = lf[sid]['qb_years']
+            max_mo = dq[sid]['max_mo']
+            min_mo = dq[sid]['min_mo']
+            max_median_q = dq[sid]['max_median_q']
+            min_median_q = dq[sid]['min_median_q']
+
             new_metadata[sid] = metadata[sid]
+
+            new_metadata[sid]['max_mo'] = max_mo
+            new_metadata[sid]['min_mo'] = min_mo
+            new_metadata[sid]['max_median_q'] = max_median_q
+            new_metadata[sid]['min_median_q'] = min_median_q
             new_metadata[sid]['irr_mean'] = mean_
             new_metadata[sid]['irr_std'] = std
             new_metadata[sid]['irr_normstd'] = norm_std
             new_metadata[sid]['lf_m'] = lowflo
             new_metadata[sid]['qb_years'] = qb_yrs
-        except KeyError:
+
+        except KeyError as e:
+            print(sid, e)
             pass
     with open(metadata_out, 'w') as f:
         json.dump(new_metadata, f)
@@ -550,78 +619,89 @@ def write_json_to_shapefile(in_shp, out_shp, meta):
 
 def baseflow_correlation_search(climate_dir, in_json, out_json):
     """Find linear relationship between climate and flow in an expanding time window"""
-    l = [os.path.join(climate_dir, x) for x in os.listdir(climate_dir)]
+    l = sorted([os.path.join(climate_dir, x) for x in os.listdir(climate_dir)])
     offsets = [x for x in range(1, 61)]
     windows = {}
-    annual_response, irrigated, irrigated_annual_resp = 0, 0, 0
     lags = []
+    # 06024450
+    month_series = [x for x in range(1, 13)] + [x for x in range(1, 13)]
+    date_sample = None
     with open(in_json, 'r') as f:
         metadata = json.load(f)
     for csv in l:
         try:
-            station = os.path.basename(csv).strip('.csv')
-            print('\n{}'.format(station))
+            sid = os.path.basename(csv).strip('.csv')
+            s_meta = metadata[sid]
+            print('\n{}'.format(sid))
+            max_q_mo, min_q_mo = s_meta['max_mo'], s_meta['min_mo']
+            if min_q_mo < max_q_mo:
+                recession_months = month_series[max_q_mo: min_q_mo + 12]
+            else:
+                recession_months = month_series[max_q_mo: min_q_mo + 1]
+            print(max_q_mo, min_q_mo, recession_months)
+            irr = s_meta['irr_mean']
             df = hydrograph(csv)
             df['ai'] = (df['etr'] - df['ppt']) / (df['etr'] + df['ppt'])
             ai = df['ai']
-            for m in range(1, 13):
-                qb = df[df.index.month.isin([m])]['qb'].dropna().resample('A').agg(DataFrame.sum, skipna=False)
-                years = [x.year for x in qb.index]
-                corr = (0, 0.0)
-                for lag in offsets:
-                    dates = [(date(y, m, 1), date(y, m, 1) + rdlt(months=-lag)) for y in
-                             years]
-                    ind = [ai[d[1]: d[0]].sum() for d in dates]
-                    lr = linregress(ind, qb)
-                    r, p = lr.rvalue, lr.pvalue
-                    if abs(r) > corr[1] and p < 0.05:
-                        corr = (lag, abs(r))
-                        d = {'STANAME': metadata[station]['STANAME'], 'AREA_SQKM': metadata[station]['AREA_SQKM'],
-                             'qb_month': m, 'lag': lag, 'r': r, 'pval': p, 'irr': metadata[station]['irr']}
+
+            qb = df[df.index.month.isin(recession_months)]['qb'].dropna().resample('A').agg(DataFrame.sum,
+                                                                                            skipna=False)
+            years = [x.year for x in qb.index]
+            corr = (0, 0.0)
+            found_sig = False
+            for lag in offsets:
+                dates = [(date(y, recession_months[0], 1), date(y, recession_months[0], 1) + rdlt(months=-lag)) for y in
+                         years]
+                ind = [ai[d[1]: d[0]].sum() for d in dates]
+                lr = linregress(ind, qb)
+                r, p = lr.rvalue, lr.pvalue
+                if abs(r) > corr[1]:
+                    corr = (lag, abs(r))
+                    d = {'STANAME': s_meta['STANAME'], 'qb_month': m,
+                         'lag': lag, 'r': r, 'pval': p, 'recession_months': recession_months}
+                    date_sample = dates[0]
+                    # print('month {}'.format(d['qb_month']), 'lag {}'.format(d['lag']), 'r {:.2f}'.format(d['r']),
+                    #       '\np {:.2f}'.format(d['pval']), d['STANAME'], '{:.3f}'.format(irr), date_sample)
+                    found_sig = True
+
+            if not found_sig:
+                print('no significant r at {}'.format(sid, s_meta['STANAME']))
+                continue
 
             print('month {}'.format(d['qb_month']), 'lag {}'.format(d['lag']), 'r {:.2f}'.format(d['r']),
-                  '\np {:.2f}'.format(d['pval']), d['STANAME'], '{:.3f}'.format(metadata[station]['irr']), dates[0])
-            windows[station] = d
-            if d['lag'] < 25:
-                annual_response += 1
-            if metadata[station]['irr'] > 0.01:
-                irrigated += 1
-            if d['lag'] < 25 and metadata[station]['irr'] > 0.01:
-                irrigated_annual_resp += 1
-                lags.append(d['lag'])
+                  '\np {:.2f}'.format(d['pval']), d['STANAME'], '{:.3f}'.format(irr), date_sample)
 
+            windows[sid] = {**d, **s_meta}
+            if irr > 0.01:
+                lags.append(d['lag'])
         except Exception as e:
-            print(csv, 'failed', e)
+            print(csv, e)
+
+    pprint(sorted(Counter(lags).items()))
 
     with open(out_json, 'w') as f:
         json.dump(windows, f)
-    print('{} annual response, {} irrigated, {} annual response and irrigated'.format(annual_response, irrigated,
-                                                                                      irrigated_annual_resp))
-    pprint(sorted(Counter(lags).items()))
+
+
 
 
 if __name__ == '__main__':
+    dq = '/media/research/IrrigationGIS/gages/hydrographs/daily_q_bf'
     i_json = '/media/research/IrrigationGIS/gages/station_metadata/metadata.json'
     o_json = '/media/research/IrrigationGIS/gages/station_metadata/basin_irr_lf.json'
     mq = '/media/research/IrrigationGIS/gages/hydrographs/q_bf_monthly'
     m = 1
     irr_data = '/media/research/IrrigationGIS/gages/ee_exports/series/extracts_comp_25AUG2021.csv'
-    get_basin_meatadata(i_json, mq, irr_data, o_json, m)
+    # get_basin_metadata(i_json, mq, dq, irr_data, o_json, m)
 
-    # o_json = '/media/research/IrrigationGIS/gages/station_metadata/metadata_qbf.json'
-    # daily_q = '/media/research/IrrigationGIS/gages/hydrographs/daily_q'
-    # dst = '/media/research/IrrigationGIS/gages/hydrographs/daily_q_bf'
-    # write_flow_parameters(daily_q, i_json, dst, o_json)
-
-    # clim_dir = '/media/research/IrrigationGIS/gages/merged_q_ee/q_terraclim'
-    # i_json = '/media/research/IrrigationGIS/gages/station_metadata/irr_areas.json'
-    # o_json = '/media/research/IrrigationGIS/gages/station_metadata/climresponse_qbJASO_fromOct1.json'
+    clim_dir = '/media/research/IrrigationGIS/gages/merged_q_ee/q_terraclim'
+    # i_json = '/media/research/IrrigationGIS/gages/station_metadata/basin_irr_lf.json'
+    # o_json = '/media/research/IrrigationGIS/gages/station_metadata/basin_lag_recessoion.json'
     # baseflow_correlation_search(clim_dir, i_json, o_json)
 
-    # i_json = '/media/research/IrrigationGIS/gages/station_metadata/irr_impacted_metadata_25AUG2021.json'
+    # i_json = '/media/research/IrrigationGIS/gages/station_metadata/irr_impacted_metadata_31AUG2021.json'
     # i_shp = '/media/research/IrrigationGIS/gages/watersheds/selected_watersheds_meta.shp'
-    # o_shp = '/media/research/IrrigationGIS/gages/watersheds/response_qbm_25AUG2021.shp'
+    # o_shp = '/media/research/IrrigationGIS/gages/watersheds/response_31AUG2021.shp'
     # write_json_to_shapefile(i_shp, o_shp, i_json)
-    # monthly = '/media/research/IrrigationGIS/gages/hydrographs/q_bf_monthly'
-    # low_flow_months(monthly)
+
 # ========================= EOF ====================================================================
