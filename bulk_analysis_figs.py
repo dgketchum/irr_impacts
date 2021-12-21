correct climate-q slope normalization

bulk_analysis_figs.py
@@ -70,7 +70,7 @@ def nass_irrmapper_climate(irr_dir, nass_c, fig_dir):
 
     m_start, m_end = 10, 9
     years = [x for x in range(1986, 2021)]
-    clim_dates = [(date(y - 1, 10, 1),  date(y, 9, monthrange(y, m_end)[1])) for y in years]
+    clim_dates = [(date(y, 4, 1),  date(y, 9, monthrange(y, m_end)[1])) for y in years]
     cc_dates = [(date(y, 5, 1), date(y, 10, 31)) for y in years]
     irr_dates = [(date(y, 7, 1), date(y, 7, 31)) for y in years]
 
@@ -92,7 +92,7 @@ def nass_irrmapper_climate(irr_dir, nass_c, fig_dir):
         cc = np.array([idf['cc'][d[0]: d[1]].sum() for d in cc_dates])
         cc[cc == 0.0] = np.nan
         irr = np.array([idf['irr'][d[0]: d[1]].sum() for d in irr_dates]) / 4046.86
-        if np.any(irr < 10000):
+        if np.any(irr[5:] < 1000):
             continue
         nrow = [(k[-4:], v) for k, v in ndf.loc[co].items() if 'VALUE' in k]
         n_v, n_y = [x[1] for x in nrow], [int(x[0]) for x in nrow]
@@ -107,13 +107,13 @@ def nass_irrmapper_climate(irr_dir, nass_c, fig_dir):
         ax[0].set(ylabel='Acres Irrigated')
         ax[0].set_xlim(years[0], years[-1])
         ax[1].plot(years, ppt, color='blue')
-        ax[1].set(ylabel='WY Precipitation [m^3]')
+        ax[1].set(ylabel='AMJJA Precipitation [m^3]')
         ax[1].set_xlim(years[0], years[-1])
         ax[2].plot(years, cc, color='black')
         ax[2].set(ylabel='Crop Consumption [m^3]')
         ax[2].set_xlim(years[0], years[-1])
         ax[3].plot(years, ai, color='red')
-        ax[3].set(ylabel='Aridity Index [-]')
+        ax[3].set(ylabel='AMJJA Aridity Index [-]')
         ax[3].set_xlim(years[0], years[-1])
 
         plt.suptitle('{} Co. {}'.format(co_str, st_str))

figures.py
@@ -188,17 +188,17 @@ def trends_panel(irr_impact, png):
         impact_keys = [p for p, v in d.items() if isinstance(v, dict)]
 
         [clime_slope.append(irr_impact_d[s][k]['clim_slope']) for k in d.keys() if k in impact_keys]
-        [clim_r.append(irr_impact_d[s][k]['clim_rsq']) for k in d.keys() if k in impact_keys]
+        [clim_r.append(irr_impact_d[s][k]['clim_r']) for k in d.keys() if k in impact_keys]
 
         [resid_slope.append(irr_impact_d[s][k]['resid_slope']) for k in d.keys() if k in impact_keys]
-        [resid_r.append(irr_impact_d[s][k]['resid_rsq']) for k in d.keys() if k in impact_keys]
+        [resid_r.append(irr_impact_d[s][k]['resid_r']) for k in d.keys() if k in impact_keys]
         # TODO: write q_resid_time m/r/p and cci_time m/r/p to json from gage analysis
         for k, v in d.items():
             if k in impact_keys:
 
-                lr_cc = linregress(years, d[k]['cci_data'])
+                lr_cc = linregress(years, d[k]['cc_data'])
                 t_cci_r, t_cci_slope, t_cci_p = lr_cc.rvalue, lr_cc.slope, lr_cc.pvalue
-                t_cci_slope_norm = lr_cc.slope * np.std(years) / np.std(d[k]['cci_data'])
+                t_cci_slope_norm = lr_cc.slope * np.std(years) / np.std(d[k]['cc_data'])
                 if t_cci_p > 0.05:
                     continue
                 lr_q_t = linregress(years, d[k]['q_resid_line'])
@@ -213,13 +213,13 @@ def trends_panel(irr_impact, png):
     n_bins = 30
 
     bin_clime_r, bin_clime_slope = np.linspace(0.1, 0.9, n_bins), \
-                                   np.linspace(-0.5, 0.2, n_bins)
+                                   np.linspace(-0.9, 0.75, n_bins)
 
     bin_resid_r, bin_resid_slope = np.linspace(-0.7, 0.7, n_bins), \
                                    np.linspace(-0.7, 0.7, n_bins)
 
     bin_t_cci_r, bin_t_cci_slope = np.linspace(-0.6, 0.8, n_bins), \
-                                   np.linspace(-0.01, 0.015, n_bins)
+                                   np.linspace(-0.66, 0.95, n_bins)
 
     bin_t_q_res_r, bin_t_q_res_slope = np.linspace(-0.8, 0.8, n_bins), \
                                    np.linspace(-0.8, 0.8, n_bins)
@@ -230,7 +230,7 @@ def trends_panel(irr_impact, png):
         impact_keys = [p for p, v in d.items() if isinstance(v, dict)]
         for k, v in d.items():
             if k in impact_keys:
-                lr_cc = linregress(years, d[k]['cci_data'])
+                lr_cc = linregress(years, d[k]['cc_data'])
                 t_cci_r, t_cci_slope, t_cci_p = lr_cc.rvalue, lr_cc.slope, lr_cc.pvalue
                 lr_q_t = linregress(years, d[k]['q_resid_line'])
                 t_q_r, t_q_slope, t_q_p = lr_q_t.rvalue, lr_q_t.slope, lr_q_t.pvalue
@@ -257,7 +257,7 @@ def trends_panel(irr_impact, png):
                     ax[2, 0].set(ylabel='count')
 
                     # residual flow - crop consumption data
-                    cci, resid, resid_line = v['cci_data'], v['q_resid'], v['q_resid_line']
+                    cci, resid, resid_line = v['cc_data'], v['q_resid'], v['q_resid_line']
                     ax[0, 1].set(xlabel='cci [m]')
                     ax[0, 1].set(ylabel='q residual [m^3]')
                     ax[0, 1].scatter(cci, resid)
@@ -272,7 +272,7 @@ def trends_panel(irr_impact, png):
                     ax[2, 1].set(ylabel='count')
 
                     # crop consumption trend data
-                    cc = v['cci_data']
+                    cc = v['cc_data']
                     cc_line = (lr_cc.slope) * np.array(years) + lr_cc.intercept
                     ax[0, 2].set(xlabel='Year')
                     ax[0, 2].set(ylabel='cc [m^3]')
@@ -310,7 +310,6 @@ def trends_panel(irr_impact, png):
 
 
 if __name__ == '__main__':
-    matplotlib.use('TkAgg')
     root = '/media/research/IrrigationGIS/gages'
     if not os.path.exists(root):
         root = '/home/dgketchum/data/IrrigationGIS/gages'
@@ -329,12 +328,8 @@ if __name__ == '__main__':
     # scatter_figs = os.path.join(figs, 'scatter_area_v_climate_response')
     # response_time_to_area(c_json, fig_dir=figs)
 
-    clim_resp = os.path.join(root, 'station_metadata/basin_climate_response_all.json')
-    trend_metatdata_dir = os.path.join(root, 'station_metadata/significant_gt_2000sqkm')
-    cc_trends = os.path.join(trend_metatdata_dir, 'sig_trends_cc.json')
-    irr_resp = os.path.join(root, 'station_metadata/irr_impacted_all.json')
-    clim_dir = os.path.join(root, 'merged_q_ee/monthly_ssebop_tc_q_Comp_16DEC2021')
-    fig_dir = os.path.join(figs, 'panels')
+    irr_resp = os.path.join(root, 'station_metadata/cc_impacted.json')
+    fig_dir = os.path.join(figs, 'panels_cc')
     trends_panel(irr_impact=irr_resp, png=fig_dir)
 
 # ========================= EOF ====================================================================

gage_analysis.py
@@ -351,7 +351,7 @@ def get_sig_irr_impact(metadata, ee_series, out_jsn=None, fig_dir=None, gage_exa
                     ols = sm.OLS(resid, years_c)
                     fit_resid_q = ols.fit()
                     lr = linregress(resid, years)
-                    flow_r, = (lr.rvalue).item()
+                    flow_r = (lr.rvalue).item()
                     resid_p_q = (fit_resid_q.pvalues[1]).item()
                     resid_line_q = fit_resid_q.params[1] * np.array(years) + fit_resid_q.params[0]
                     sig_stations[sid]['{}-{}'.format(cc_start, cc_end)].update({'q_time_r': flow_r,

tables.py
@@ -60,12 +60,17 @@ def merge_ssebop_tc_q(extracts, out_dir, flow_dir, glob='glob', division='basins
             irr, cc, et = [], [], []
             for y, m in months:
                 try:
-                    cc.append(d['cc_{}_{}'.format(y, m)])
-                    et.append(d['et_{}_{}'.format(y, m)])
-                    irr.append(d['irr_{}_{}'.format(y, m)])
+                    cc_, et_ = d['cc_{}_{}'.format(y, m)], d['et_{}_{}'.format(y, m)]
+                    cc.append(cc_)
+                    et.append(et_)
                 except KeyError:
                     cc.append(np.nan)
                     et.append(np.nan)
+
+                try:
+                    irr_ = d['irr_{}_{}'.format(y, m)]
+                    irr.append(irr_)
+                except KeyError:
                     irr.append(np.nan)
 
             irr = irr, 'irr'
@@ -113,8 +118,8 @@ if __name__ == '__main__':
     # g = 'Comp_14DEC2021'
     # merge_ssebop_tc_q(extract_, gage_src, out_dir, glob=g)
 
-    extract_ = os.path.join(root, 'time_series/counties_IrrMapperComp_17DEC2021/ee_export')
-    out = os.path.join(root, 'time_series/counties_IrrMapperComp_17DEC2021/county_monthly')
-    g = 'County_Comp_14DEC2021'
+    extract_ = os.path.join(root, 'time_series/counties_IrrMapperSW_21DEC2021/ee_export')
+    out = os.path.join(root, 'time_series/counties_IrrMapperSW_21DEC2021/county_monthly')
+    g = 'County_sw_20DEC2021'
     merge_ssebop_tc_q(extract_, out_dir=out, flow_dir=None, glob=g, division='county', join_key='GEOID')
 # ========================= EOF ====================================================================