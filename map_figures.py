08f7f053f9535eb7a6ab0cd3c6cc4d952f1a8953
dgketchum <pdWs7kyy>
2021-12-28 11:14:26 -0700

    map one plotting correctly

--- map_figures.py
+++ map_figures.py
@@ -1,12 +1,12 @@
 import os
 
-import seaborn as sns
-import pandas as pd
 import geopandas as gpd
-import pysal as ps
 import numpy as np
 import rasterio
+from skimage.filters.rank import majority
+from skimage.morphology import disk
 from rasterio.plot import show
+import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 
 
@@ -14,32 +14,38 @@ def map_fig_one(basins_shp, irrmap, gages_shp, states, all_gages, png):
     adf = gpd.read_file(all_gages)
     sdf = gpd.read_file(states)
     bdf = gpd.read_file(basins_shp)
+
     src = rasterio.open(irrmap)
     left, bottom, right, top = src.bounds
-    irr = src.read()[0, :, :]
-    irr = np.ma.masked_where(irr == 0,
+    irr = src.read()[0, :, :].astype(np.dtype('uint8'))
+    src.close()
+    irr = majority(irr, disk(2))
+    irr = np.ma.masked_where(irr < 5,
                              irr,
                              copy=True)
 
     gdf = gpd.read_file(gages_shp)
     gdf['import'] = (gdf['cc_q'].values > 0.2) & (gdf['AREA'].values < 7000.)
-    gdf = gdf[gdf['import'] == False]
+    gdf = gdf[gdf['import'] == 0]
     gdf = gdf[gdf['cc_q'] >= 0.0]
     gdf = gdf[gdf['cc_q'] <= 0.5]
     gdf['rank'] = gdf['cc_q'].rank(ascending=True)
 
-    fig, ax = plt.subplots(1, figsize=(80, 64))
-    show(irr, cmap=plt.cm.coolwarm, alpha=0.1, ax=ax, extent=(left, right, bottom, top))
-    gdf.plot(column='rank', cmap=plt.cm.coolwarm, legend=True, scheme='quantiles', ax=ax)
-    bdf.geometry.boundary.plot(color=None, edgecolor='purple', linewidth=2, ax=ax)
+    fig, ax = plt.subplots(1, figsize=(20, 16))
+    show(irr.data, cmap='RdBu', alpha=0.5, ax=ax, extent=(left, right, bottom, top),
+         norm=colors.Normalize(vmin=-1.0, vmax=1.0),)
     sdf.geometry.boundary.plot(color=None, edgecolor='k', linewidth=1, alpha=0.8, ax=ax)
+    bdf.geometry.boundary.plot(color=None, edgecolor='purple', linewidth=2, ax=ax)
     adf.plot(color='k', linewidth=0, alpha=0.1, ax=ax)
+    gdf.plot(column='rank', linewidth=1., cmap=plt.cm.coolwarm, legend=True, scheme='quantiles', ax=ax)
     # locs, labels = plt.xticks()
     # plt.setp(labels, rotation=90)
-    plt.axis('equal')
-    plt.savefig(png)
-    # plt.show()
-    plt.close()
+    # plt.axis('equal')
+    # plt.savefig(png)
+    plt.xlim(-125, -102)
+    plt.ylim(30, 50)
+    plt.show()
+    # plt.close()
 
 
 if __name__ == '__main__':
@@ -53,7 +59,7 @@ if __name__ == '__main__':
     basins = os.path.join(fig_shp, 'study_basins.shp')
     states_ = os.path.join(fig_shp, 'western_11_states.shp')
     all_gages_ = os.path.join(fig_shp, 'study_gages_all.shp')
-    irrmapper = os.path.join(fig_tif, 'irr_change_1990_2020_150m.tif')
+    irrmapper = os.path.join(fig_tif, 'irr_freq_merge_360m.tif')
     fig = os.path.join(fig_data, 'map_fig_one.png')
     map_fig_one(basins, irrmapper, gages, states_, all_gages_, fig)
 # ========================= EOF ====================================================================
