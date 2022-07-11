4b40776c97eb15b89f76362bedf2baef45bc5085
dgketchum <pdWs7kyy>
2022-07-11 12:37:07 -0600

    refactor trends analysis and cc vs qres into seperate functions

--- bayes_analysis.py
+++ bayes_analysis.py
@@ -17,6 +17,16 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # temporary hack to open pickled model with renamed module
 sys.modules['linear_regression_errors'] = bayes_models
 
+SAMPLE_KWARGS = {'draws': 500,
+                 'tune': 5000,
+                 'target_accept': 0.9,
+                 'cores': None,
+                 'init': 'advi+adapt_diag',
+                 'chains': 4,
+                 'n_init': 50000,
+                 'progressbar': False,
+                 'return_inferencedata': False}
+
 
 def standardize(arr):
     arr = (arr - arr.mean()) / arr.std()
@@ -28,7 +38,96 @@ def magnitude(arr):
     return 10 ** mag
 
 
-def run_bayes_regression(traces_dir, stations, multiproc=False):
+def run_bayes_regression_trends(traces_dir, stations, multiproc=False):
+    if not os.path.exists(traces_dir):
+        os.makedirs(traces_dir)
+
+    with open(stations, 'r') as f:
+        stations = json.load(f)
+
+    if multiproc:
+        pool = Pool(processes=30)
+
+    covered = []
+    for sid, rec in stations.items():
+
+        if sid in covered:
+            continue
+
+        if sid != '06142400':
+            continue
+
+        covered.append(sid)
+
+        if not multiproc:
+            bayes_linear_regression_trends(sid, rec, float(qres_err_),
+                                           float(cc_err_), trace_dir, 4)
+        else:
+            pool.apply_async(bayes_linear_regression_trends, args=(sid, rec, float(qres_err_),
+                                                                   float(cc_err_), traces_dir, 1))
+
+    if multiproc:
+        pool.close()
+        pool.join()
+
+
+def bayes_linear_regression_trends(station, records, qres_err, cc_err, trc_dir, cores):
+    try:
+        cc = np.array(records['cc_data'])
+        qres = np.array(records['qres_data'])
+        ai = np.array(records['ai_data'])
+        q = np.array(records['q_data'])
+
+        sample_kwargs = SAMPLE_KWARGS
+        sample_kwargs['cores'] = cores
+
+        q = (q - q.min()) / (q.max() - q.min()) + 0.001
+        ai = (ai - ai.min()) / (ai.max() - ai.min()) + 0.001
+        cc = (cc - cc.min()) / (cc.max() - cc.min()) + 0.001
+        qres = (qres - qres.min()) / (qres.max() - qres.min()) + 0.001
+        years = (np.linspace(0, 1, len(qres)) + 0.001).reshape(1, -1) + 0.001
+
+        qres_err = qres_err * np.ones_like(qres) * 0.5
+        cc_err *= 0.5
+
+        regression_combs = [(years, cc, None, cc_err),
+                            (years, qres, None, qres_err),
+                            (years, ai, None, qres_err),
+                            (years, q, None, qres_err * 0.)]
+
+        trc_subdirs = ['time_cc', 'time_qres', 'time_ai', 'time_q']
+
+        for subdir in trc_subdirs:
+            model_dir = os.path.join(trc_dir, subdir)
+            if not os.path.isdir(model_dir):
+                os.makedirs(model_dir)
+
+        for (x, y, x_err, y_err), subdir in zip(regression_combs, trc_subdirs):
+
+            if int(records['q_window'][0]) not in range(4, 11):
+                continue
+            if records[subdir]['p'] > 0.05:
+                continue
+
+            save_model = os.path.join(trc_dir, subdir, '{}_q_{}.model'.format(station, records['q_window']))
+            if os.path.isfile(save_model):
+                print('{} exists'.format(save_model))
+                continue
+            else:
+                print('\n==================== sampling {} q {} at {}, p = {:.3f} ======================='.format(
+                    subdir, records['q_window'], station, records[subdir]['p']))
+
+            model = LinearModel()
+
+            model.fit(x, y, y_err, x_err,
+                      save_model=save_model,
+                      sample_kwargs=sample_kwargs)
+
+    except Exception as e:
+        print(e, station, records['q_window'])
+
+
+def run_bayes_regression_cc_qres(traces_dir, stations, multiproc=False):
     if not os.path.exists(traces_dir):
         os.makedirs(traces_dir)
 
@@ -47,81 +146,60 @@ def run_bayes_regression(traces_dir, stations, multiproc=False):
         if sid in covered:
             continue
 
-        if rec['res_sig'] > 0.05:
-            # print(sid, 'lr insig')
+        if sid != '06142400':
+            continue
+
+        if rec['p'] > 0.05:
             continue
 
         covered.append(sid)
 
         if not multiproc:
-            bayes_linear_regression(sid, rec, per, float(qres_err_),
-                                    float(cc_err_), trace_dir, 4)
+            bayes_linear_regression_cc_qres(sid, rec, per, float(qres_err_),
+                                            float(cc_err_), trace_dir, 4)
         else:
-            pool.apply_async(bayes_linear_regression, args=(sid, rec, per, float(qres_err_),
-                                                            float(cc_err_), traces_dir, 1))
+            pool.apply_async(bayes_linear_regression_cc_qres, args=(sid, rec, per, float(qres_err_),
+                                                                    float(cc_err_), traces_dir, 1))
 
     if multiproc:
         pool.close()
         pool.join()
 
 
-def bayes_linear_regression(station, records, period, qres_err, cc_err, trc_dir, cores):
+def bayes_linear_regression_cc_qres(station, records, period, qres_err, cc_err, trc_dir, cores):
     try:
-        cc = np.array(records['cc_data']).reshape(1, len(records['cc_data']))
-        qres = np.array(records['q_resid'])
-        ai = np.array(records['ai_data'])
+        cc = np.array(records['cc_data'])
+        qres = np.array(records['qres_data'])
 
         cc = (cc - cc.min()) / (cc.max() - cc.min()) + 0.001
         qres = (qres - qres.min()) / (qres.max() - qres.min()) + 0.001
-        years = (np.linspace(0, 1, len(qres)) + 0.001).reshape(1, -1) + 0.001
 
         qres_err = qres_err * np.ones_like(qres) * 0.5
         cc_err *= 0.5
 
-        sample_kwargs = {'draws': 500,
-                         'tune': 5000,
-                         'target_accept': 0.9,
-                         'cores': cores,
-                         'init': 'advi+adapt_diag',
-                         'chains': 4,
-                         'n_init': 50000,
-                         'progressbar': False,
-                         'return_inferencedata': False}
+        sample_kwargs = SAMPLE_KWARGS
+        sample_kwargs['cores'] = cores
 
-        regression_combs = [(cc, qres, cc_err, qres_err),
-                            (years, cc, None, cc_err),
-                            (years, qres, None, qres_err),
-                            (years, ai, None, qres_err)]
+        model_dir = os.path.join(trc_dir, 'cc_qres')
+        if not os.path.isdir(model_dir):
+            os.makedirs(model_dir)
 
-        trc_subdirs = ['cc_qres', 'time_cc', 'time_qres', 'time_ai']
+        save_model = os.path.join(model_dir, '{}_cc_{}_q_{}.model'.format(station,
+                                                                          period,
+                                                                          records['q_window']))
+        if os.path.isfile(save_model):
+            print('{} exists'.format(save_model))
+            return None
 
-        for subdir in trc_subdirs:
-            model_dir = os.path.join(trc_dir, subdir)
-            if not os.path.isdir(model_dir):
-                os.makedirs(model_dir)
+        else:
+            print('\n==================== sampling q {} at {}, p = {:.3f} ======================='.format(
+                records['q_window'], station, records['p']))
 
-        for (x, y, x_err, y_err), subdir in zip(regression_combs[:1], trc_subdirs[:1]):
-            if subdir == 'time_cc':
-                y = y[0, :]
+        model = LinearRegressionwithErrors()
 
-            save_model = os.path.join(trc_dir, subdir, '{}_cc_{}_q_{}.model'.format(station,
-                                                                                    period,
-                                                                                    records['q_window']))
-            if os.path.isfile(save_model):
-                print('{} exists'.format(save_model))
-                continue
-            else:
-                print('\n==================== sampling q {} at {}, p = {:.3f} ======================='.format(
-                    records['q_window'], station, records['res_sig']))
-
-            if subdir == 'cc_qres':
-                model = LinearRegressionwithErrors()
-            else:
-                model = LinearModel()
-
-            model.fit(x, y, y_err, x_err,
-                      save_model=save_model,
-                      sample_kwargs=sample_kwargs)
+        model.fit(cc, qres, cc_err, qres_err,
+                  save_model=save_model,
+                  sample_kwargs=sample_kwargs)
 
     except Exception as e:
         print(e, station, period)
@@ -216,17 +294,21 @@ if __name__ == '__main__':
         state = 'm_{}_cc_{}_qreserr_{}'.format(m, str(cc_err_), str(qres_err_))
 
         trace_dir = os.path.join(root, 'traces', state)
+        analysis_d = os.path.join(root, 'analysis')
 
-        f_json = os.path.join(root, 'analysis', 'impacts_{}_cc_test.json'.format(m))
+        monthly_json = os.path.join(analysis_d, 'trends_{}.json'.format(m))
 
         if not os.path.exists(trace_dir):
             os.makedirs(trace_dir)
 
-        run_bayes_regression(trace_dir, f_json, multiproc=True)
+        run_bayes_regression_trends(trace_dir, monthly_json, multiproc=True)
 
+        f_json = os.path.join(analysis_d, 'qres_cc_{}.json'.format(m))
 
-        o_json = os.path.join(root, 'station_metadata', 'flowtrends',
-                              'bayes_trend_{}.json'.format(state))
-        bayes_sig_irr_impact(f_json, trace_dir, o_json, update=False)
+        run_bayes_regression_cc_qres(trace_dir, f_json, multiproc=True)
+
+        # o_json = os.path.join(root, 'station_metadata', 'flowtrends',
+        #                       'bayes_trend_{}.json'.format(state))
+        # bayes_sig_irr_impact(f_json, trace_dir, o_json, update=False)
 
 # ========================= EOF ====================================================================
