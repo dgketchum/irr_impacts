bca96d128241e3694cc49b1e8827d82147b99923
dgketchum <dgketchum@gmail.com>
2022-10-22 11:56:49 -0600

    export both GridMET and TerraClimate, 1982 - 2021

--- gridded_data.py
+++ gridded_data.py
@@ -2,17 +2,22 @@ import os
 import sys
 from calendar import monthrange
 
+import numpy as np
 import ee
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 sys.setrecursionlimit(5000)
 
 RF_ASSET = 'projects/ee-dgketchum/assets/IrrMapper/IrrMapperComp'
-BASINS = 'users/dgketchum/gages/gage_basins'
 UMRB_CLIP = 'users/dgketchum/boundaries/umrb_ylstn_clip'
 CMBRB_CLIP = 'users/dgketchum/boundaries/CMB_RB_CLIP'
 CORB_CLIP = 'users/dgketchum/boundaries/CO_RB'
-BUCKET = 'wudr'
+
+try:
+    ee.Initialize()
+    print('Authorized')
+except Exception as e:
+    print('You are not authorized: {}'.format(e))
 
 ET_ASSET = ee.ImageCollection('users/dgketchum/ssebop/cmbrb')
 
@@ -28,14 +33,16 @@ NAVAJO = ee.Geometry.Polygon([[-108.50192867920967, 36.38701227276218],
                               [-108.50192867920967, 36.78068624960868],
                               [-108.50192867920967, 36.38701227276218]])
 
+TEST_POINT = ee.Geometry.Point(-111.19206055457778, 45.587493372544984)
 
-def export_gridded_data(tables, years=None, description=None,
-                        min_years=0, basins=True):
+
+def export_gridded_data(tables, bucket, years, description, min_years=0, debug=False):
     """
     Reduce Regions, i.e. zonal stats: takes a statistic from a raster within the bounds of a vector.
     Use this to get e.g. irrigated area within a county, HUC, or state. This can mask based on Crop Data Layer,
     and can mask data where the sum of irrigated years is less than min_years. This will output a .csv to
     GCS wudr bucket.
+    :param bucket:
     :param tables: vector data over which to take raster statistics
     :param years: years over which to run the stats
     :param description: export name append str
@@ -48,16 +55,13 @@ def export_gridded_data(tables, years=None, description=None,
     umrb_clip = ee.FeatureCollection(UMRB_CLIP)
     corb_clip = ee.FeatureCollection(CORB_CLIP)
 
-    # fc = ee.FeatureCollection(ee.FeatureCollection(tables).filter(ee.Filter.eq('STAID', '12484500')))
-
     irr_coll = ee.ImageCollection(RF_ASSET)
-    coll = irr_coll.filterDate('1991-01-01', '2020-12-31').select('classification')
+    coll = irr_coll.filterDate('1987-01-01', '2021-12-31').select('classification')
     remap = coll.map(lambda img: img.lt(1))
     irr_min_yr_mask = remap.sum().gt(min_years)
-    # sum = remap.sum().mask(irr_mask)
 
     for yr in years:
-        for month in range(4, 11):
+        for month in range(1, 13):
             s = '{}-{}-01'.format(yr, str(month).rjust(2, '0'))
             end_day = monthrange(yr, month)[1]
             e = '{}-{}-{}'.format(yr, str(month).rjust(2, '0'), end_day)
@@ -90,11 +94,15 @@ def export_gridded_data(tables, years=None, description=None,
             etr = tclime_sums.select('pet_sum').multiply(0.0001)
             swb_aet = tclime_sums.select('aet_sum').mask(irr_mask).multiply(0.0001)
 
+            gm_ppt, gm_etr = extract_gridmet_monthly(yr, month)
+
             irr_mask = irr_mask.reproject(crs='EPSG:5070', scale=30)
             et = et.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
             ppt = ppt.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
             etr = etr.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
             swb_aet = swb_aet.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
+            gm_ppt = gm_ppt.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
+            gm_etr = gm_etr.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
 
             cc = et.subtract(swb_aet)
 
@@ -104,32 +112,33 @@ def export_gridded_data(tables, years=None, description=None,
             cc = cc.multiply(area).rename('cc')
             ppt = ppt.multiply(area).rename('ppt')
             etr = etr.multiply(area).rename('etr')
+            gm_ppt = gm_ppt.multiply(area).rename('gm_ppt')
+            gm_etr = gm_etr.multiply(area).rename('gm_etr')
             swb_aet = swb_aet.multiply(area).rename('swb_aet')
 
-            if basins:
-                selector = ['STAID']
+            if yr > 1986 and month in np.arange(4, 11):
+                bands = irr.addBands([et, cc, ppt, etr, swb_aet, gm_ppt, gm_etr])
+                select_ = ['STAID', 'irr', 'et', 'cc', 'ppt', 'etr', 'swb_aet', 'gm_ppt', 'gm_etr']
             else:
-                selector = ['GEOID', 'STUDYINT']
-
-            if yr > 1990 and month in [x for x in range(4, 11)]:
-                bands = irr.addBands([et, cc, ppt, etr, swb_aet])
-                select_ = selector + ['irr', 'et', 'cc', 'ppt', 'etr', 'swb_aet']
-            else:
-                bands = irr.addBands([ppt, etr, swb_aet])
-                select_ = selector + ['ppt', 'etr', 'swb_aet', 'irr']
+                bands = ppt.addBands([etr, gm_ppt, gm_etr])
+                select_ = ['STAID', 'ppt', 'etr', 'gm_ppt', 'gm_etr']
 
             data = bands.reduceRegions(collection=fc,
                                        reducer=ee.Reducer.sum(),
                                        scale=30)
 
-            # fields = data.first().propertyNames().remove('.geo')
-            # p = data.first().getInfo()['properties']
+            if debug:
+                pt = bands.sample(region=TEST_POINT,
+                                  numPixels=1,
+                                  scale=30)
+                fields = pt.propertyNames().remove('.geo')
+                p = data.first().getInfo()['properties']
 
             out_desc = '{}_{}_{}'.format(description, yr, month)
             task = ee.batch.Export.table.toCloudStorage(
                 data,
                 description=out_desc,
-                bucket=BUCKET,
+                bucket=bucket,
                 fileNamePrefix=out_desc,
                 fileFormat='CSV',
                 selectors=select_)
@@ -137,19 +146,26 @@ def export_gridded_data(tables, years=None, description=None,
             print(out_desc)
 
 
-def export_et_images(polygon, tables, years=None, description=None,
+def extract_gridmet_monthly(year, month):
+    m_str, m_str_next = str(month).rjust(2, '0'), str(month + 1).rjust(2, '0')
+    if month == 12:
+        dataset = ee.ImageCollection('IDAHO_EPSCOR/GRIDMET').filterDate('{}-{}-01'.format(year, m_str),
+                                                                        '{}-{}-31'.format(year, m_str))
+    else:
+        dataset = ee.ImageCollection('IDAHO_EPSCOR/GRIDMET').filterDate('{}-{}-01'.format(year, m_str),
+                                                                        '{}-{}-01'.format(year, m_str_next))
+    pet = dataset.select('etr').sum().multiply(0.001).rename('gm_etr')
+    ppt = dataset.select('pr').sum().multiply(0.001).rename('gm_ppt')
+    return ppt, pet
+
+
+def export_et_images(polygon, tables, bucket, years=None, description=None,
                      min_years=0):
     """
     Reduce Regions, i.e. zonal stats: takes a statistic from a raster within the bounds of a vector.
     Use this to get e.g. irrigated area within a county, HUC, or state. This can mask based on Crop Data Layer,
     and can mask data where the sum of irrigated years is less than min_years. This will output a .csv to
     GCS wudr bucket.
-    :param tables: vector data over which to take raster statistics
-    :param years: years over which to run the stats
-    :param description: export name append str
-    :param cdl_mask:
-    :param min_years:
-    :return:
     """
     fc = ee.FeatureCollection(tables)
     cmb_clip = ee.FeatureCollection(CMBRB_CLIP)
@@ -222,7 +238,7 @@ def export_et_images(polygon, tables, years=None, description=None,
                 task = ee.batch.Export.image.toCloudStorage(
                     var_,
                     description=out_desc,
-                    bucket=BUCKET,
+                    bucket=bucket,
                     fileNamePrefix=out_desc,
                     region=polygon,
                     scale=30,
@@ -232,12 +248,12 @@ def export_et_images(polygon, tables, years=None, description=None,
                 print(out_desc)
 
 
-def export_naip(region):
+def export_naip(region, bucket):
     dataset = ee.ImageCollection('USDA/NAIP/DOQQ').filter(ee.Filter.date('2017-01-01', '2018-12-31')).mosaic()
     task = ee.batch.Export.image.toCloudStorage(
         dataset,
         description='NAIP_Navajo',
-        bucket=BUCKET,
+        bucket=bucket,
         fileNamePrefix='NAIP_Navajo',
         region=region,
         scale=30,
@@ -246,17 +262,6 @@ def export_naip(region):
     task.start()
 
 
-def is_authorized():
-    try:
-        ee.Initialize()  # investigate (use_cloud_api=True)
-        print('Authorized')
-        return True
-    except Exception as e:
-        print('You are not authorized: {}'.format(e))
-        return False
-
-
 if __name__ == '__main__':
-    is_authorized()
-
+    pass
 # ========================= EOF ================================================================================
