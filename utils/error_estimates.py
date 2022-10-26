69f1dfecdb14b02a176009cb388858edd0c14151
dgketchum <dgketchum@gmail.com>
2022-10-26 11:42:42 -0600

    initial commit script with error estimates

---
+++ utils/error_estimates.py
@@ -0,0 +1,17 @@
+import os
+
+BASIN_F1 = {'missouri': 0.8985,
+            'colorado': 0.8345,
+            'columbia': 0.8649}
+
+BASIN_CC_ERR = {'missouri': {'rmse': 0.19, 'bias': 0.06},
+                'colorado': {'rmse': 0.28, 'bias': -0.14},
+                'columbia': {'rmse': 0.23, 'bias': -0.02}}
+
+QRES_ERR = 0.198
+PPT_ERR = 0.102
+ETR_ERR = 0.153
+
+if __name__ == '__main__':
+    pass
+# ========================= EOF ====================================================================
