99f84bfacc429587c285b4184828c48453b8951e
dgketchum <dgketchum@gmail.com>
2022-10-27 09:41:22 -0600

    calculate irrmapper error for each basin

--- utils/irrmapper_error.py
+++ utils/irrmapper_error.py
@@ -1,51 +1,28 @@
 import os
 import json
+from datetime import datetime
 
-import numpy as np
 import ee
-
-from utils.gage_lists import STATION_BASINS
-
-ee.Initialize()
-
-BASIN = ['users/dgketchum/boundaries/umrb_ylstn_clip',
-         'users/dgketchum/boundaries/CMB_RB_CLIP',
-         'users/dgketchum/boundaries/CO_RB']
+import numpy as np
+import pandas as pd
 
 BASIN_MAP = {'missouri': 'users/dgketchum/boundaries/umrb_ylstn_clip',
              'columbia': 'users/dgketchum/boundaries/CMB_RB_CLIP',
              'colorado': 'users/dgketchum/boundaries/CO_RB'}
 
-# Years with at least 1000 reference fields
-BASIN_YEARS = {'missouri': [2003, 2008, 2009, 2010, 2011, 2012, 2013, 2015],
+BASIN_YEARS = {'missouri': [2008, 2009, 2010, 2011, 2012, 2013],
                'colorado': [1998, 2003, 2006, 2009, 2013, 2016],
                'columbia': [1988, 1996, 1997, 1998, 2001, 2006, 2008, 2009, 2010,
                             2011, 2012, 2013]}
 
-BASIN_F1 = {'missouri': 0.8985,
-            'colorado': 0.8345,
-            'columbia': 0.8649}
-
-BASIN_CC_ERR = {'missouri': {'rmse': 0.19, 'bias': 0.06},
-                'colorado': {'rmse': 0.28, 'bias': -0.14},
-                'columbia': {'rmse': 0.23, 'bias': -0.02}}
-
-QRES_ERR = 0.174
-PPT_ERR = 0.091
-ETR_ERR = 0.083
+BOUNDARIES = 'users/dgketchum/boundaries'
+CO_STATES = ['WY', 'CO', 'UT', 'NV', 'NM', 'AZ']
 
 
-def confusion(irr_labels, unirr_labels, irr_image, unirr_image, sid, yr, clip=False):
-    if sid in ['columbia', 'colorado', 'missouri']:
-        fc = ee.FeatureCollection(BASIN_MAP[sid])
-        domain = fc.toList(fc.size()).get(0)
-        domain = ee.Feature(domain)
-    else:
-        domain = ee.FeatureCollection('users/dgketchum/gages/gage_basins')
-        domain = domain.filterMetadata('STAID', 'equals', sid)
-        domain = ee.FeatureCollection(domain)
-        domain = domain.toList(domain.size()).get(0)
-        domain = ee.Feature(domain)
+def confusion(irr_labels, unirr_labels, irr_image, unirr_image, system, clip=False, state=None):
+    fc = ee.FeatureCollection(BASIN_MAP[system])
+    domain = fc.toList(fc.size()).get(0)
+    domain = ee.Feature(domain)
 
     true_positive = irr_image.eq(irr_labels)
     false_positive = irr_image.eq(unirr_labels)
@@ -57,6 +34,15 @@ def confusion(irr_labels, unirr_labels, irr_image, unirr_image, sid, yr, clip=Fa
         false_positive = false_positive.clip(domain)
         true_negative = true_negative.clip(domain)
         false_negative = false_negative.clip(domain)
+
+        if state:
+            fc_ = ee.FeatureCollection(os.path.join(BOUNDARIES, state))
+            state = fc_.toList(fc_.size()).get(0)
+            state = ee.Feature(state)
+            true_positive = true_positive.clip(state)
+            false_positive = false_positive.clip(state)
+            true_negative = true_negative.clip(state)
+            false_negative = false_negative.clip(state)
 
     TP = true_positive.reduceRegion(
         geometry=domain.geometry(),
@@ -136,52 +122,89 @@ def metrics(arr):
     return precision, recall
 
 
-def basin_accuracy(_dir, all_stations, out_json):
-    basin_acc = os.path.join(_dir, 'basin_json')
-    js = [os.path.join(basin_acc, x) for x in os.listdir(basin_acc) if x.endswith('.json')]
-    stations = [os.path.basename(j).split('.')[0] for j in js]
+def irrmapper_basin_acc(out_dir):
+    globe_conf = np.zeros((2, 2))
+    stations = BASIN_YEARS
+    stations = {k: [0.0, v] for k, v in stations.items()}
+    csv_out = os.path.join(out_dir, 'irrmapper_accuracy_colorado.csv')
 
-    out_acc = {}
+    cols = ['YEAR', 'STATE', 'P', 'R', 'TP', 'FP', 'FN', 'TN']
+    ind_len = 0
+    for k, v in stations.items():
+        ind_len += len(v[1])
 
-    with open(all_stations, 'r') as fp:
-        dct = json.load(fp)
+    df = pd.DataFrame(columns=cols)
 
-    sort_ = sorted([(k, v['AREA']) for k, v in dct.items()], key=lambda x: x[1], reverse=True)
-    dct = {k: dct[k] for k, v in sort_}
-
-    for sid, meta in dct.items():
-
-        if sid not in STATION_BASINS.keys():
+    ind = 0
+    _all = len(stations.keys())
+    for num, (sid, (area, years)) in enumerate(stations.items()):
+        out_json = os.path.join(out_dir, 'basin_json', '{}.json'.format(sid))
+        if not years:
             continue
+        basin_conf = np.zeros((2, 2))
+        print(sid)
+        # add for-loop over basin states if computation is timing out
+        # to clip to smaller area and break up the counting
+        for year in years:
+            row = {c: None for c in cols}
+            try:
+                row['YEAR'] = year
+                row['STAID'] = sid
 
-        if sid in stations:
-            j = js[stations.index(sid)]
-            with open(j, 'r') as f:
-                cmt = json.load(f)[sid]
-            conf = np.zeros((2, 2))
-            for pos, ct in zip([(0, 0), (0, 1), (1, 0), (1, 1)], ['TP', 'FP', 'FN', 'TN']):
-                conf[pos] += cmt[ct]
+                irr_labels, unirr_labels = create_irrigated_labels(False, int(year))
 
-            if conf.sum() == 0:
+                if not irr_labels:
+                    continue
+
+                irr_image, unirr_image = create_rf_labels(year)
+                cmt = confusion(irr_labels, unirr_labels, irr_image, unirr_image, sid, clip=True, state=None)
+                if not cmt:
+                    continue
+                if cmt['TP']['constant'] + cmt['FN']['constant'] == 0:
+                    print('no positive reference data')
+                    continue
+
+                print('{} {} {}'.format(sid, year, cmt))
+                for pos, ct in zip([(0, 0), (0, 1), (1, 0), (1, 1)], ['TP', 'FP', 'FN', 'TN']):
+                    globe_conf[pos] += cmt[ct]['constant']
+                    basin_conf[pos] += cmt[ct]['constant']
+                    row[ct] = cmt[ct]['constant']
+
+                p, r = metrics(basin_conf)
+                row['P'], row['R'] = np.round(p, decimals=3), np.round(r, decimals=3)
+
+                if np.isnan(p) or np.isnan(r):
+                    continue
+
+                df = df.append(row, ignore_index=True)
+                ind += 1
+
+            except Exception as e:
+                print(e, row['STAID'], row['YEAR'])
                 pass
-            else:
-                prec, rec = metrics(conf)
-                conf_l = list(conf.flatten())
-                ratio = (conf_l[0] + conf_l[2]) / (conf_l[1] + conf_l[3])
-                out_acc[sid] = {'f1': 2 * ((prec * rec) / (prec + rec)),
-                                'area': meta['AREA'],
-                                'STANAME': meta['STANAME'],
-                                'conf': conf_l,
-                                'ratio': ratio}
 
-        if sid not in out_acc.keys():
-            out_acc[sid] = {'f1': BASIN_F1[STATION_BASINS[sid]],
-                            'area': meta['AREA'],
-                            'STANAME': meta['STANAME'],
-                            'conf': None}
+        p, r = metrics(basin_conf)
+        print('prec {:.3f}, rec {:.3f}'.format(p, r))
+        print(print('{} {} of {}\n{}\n\n\n'.format(sid, num, _all, basin_conf)))
+        jsn = {sid: {k: v for k, v in zip(['TP', 'FP', 'FN', 'TN'], basin_conf.flatten())}}
+        with open(out_json, 'w') as f:
+            json.dump(jsn, f, indent=4)
 
-    with open(out_json, 'w') as fp:
-        json.dump(out_acc, fp, indent=4)
+        df.to_csv(csv_out.replace('.csv', '_.csv'))
+
+    print(globe_conf)
+    p, r = metrics(globe_conf)
+    df.to_csv(csv_out)
+    print('prec {}, rec {}'.format(p, r))
+    print(datetime.now())
+
+
+def initialize():
+    try:
+        ee.Initialize()
+        print('Authorized')
+    except Exception as e:
+        print('You are not authorized: {}'.format(e))
 
 
 if __name__ == '__main__':
