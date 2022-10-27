ce70ec4aab8aa3f6859fdfb6116956f1241cf1de
dgketchum <dgketchum@gmail.com>
2022-10-27 09:36:26 -0600

    update with basin-specific precip error estimates

--- utils/error_estimates.py
+++ utils/error_estimates.py
@@ -1,15 +1,17 @@
 import os
 
-BASIN_F1 = {'missouri': 0.8985,
-            'colorado': 0.8345,
-            'columbia': 0.8649}
+BASIN_IRRMAPPER_F1 = {'missouri': 0.8985,
+                      'colorado': 0.8404,
+                      'columbia': 0.8832}
 
 BASIN_CC_ERR = {'missouri': {'rmse': 0.19, 'bias': 0.06},
                 'colorado': {'rmse': 0.28, 'bias': -0.14},
                 'columbia': {'rmse': 0.23, 'bias': -0.02}}
 
-QRES_ERR = 0.198
-PPT_ERR = 0.102
+BASIN_PRECIP_RMSE = {'missouri': 0.125,
+                     'colorado': 0.146,
+                     'columbia': 0.110}
+
 ETR_ERR = 0.153
 
 if __name__ == '__main__':
