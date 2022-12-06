0039a60a507fb626887bd67c449622117468e63c
dgketchum <dgketchum@gmail.com>
2022-12-05 17:04:46 -0700

    add bivariate regression

--- utils/bayes_models.py
+++ utils/bayes_models.py
@@ -29,6 +29,35 @@ class LinearModel(LinearRegression):
                     print('saving', save_model)
 
 
+class MVLinearModel():
+
+    def __init__(self):
+        pass
+
+    def fit(self, x1, x2, y, y_error=1, x1_error=None, x2_error=None, save_model=None,
+            sample_kwargs=None):
+        with pm.Model():
+            alpha = pm.Normal('inter', 0, sd=20)
+            slope_1 = pm.Normal('slope_1', 0, sd=20)
+            slope_2 = pm.Normal('slope_2', 0, sd=20)
+
+            x1_ = pm.Normal('x_1', mu=0, sd=x1_error)
+            x2_ = pm.Normal('x_2', mu=0, sd=x2_error)
+
+            x1_hat = pm.Normal('x1_hat', mu=x1_, sd=x1_error, observed=x1)
+            x2_hat = pm.Normal('x2_hat', mu=x2_, sd=x2_error, observed=x2)
+
+            mu = alpha + slope_1 * x1_hat + slope_2 * x2_hat
+            y_hat = pm.Normal('y_hat', mu=mu, sd=y_error, observed=y)
+
+            self.trace = pm.sample(**sample_kwargs)
+
+            if save_model:
+                with open(save_model, 'wb') as buff:
+                    pickle.dump({'model': self, 'trace': self.trace}, buff)
+                    print('saving', save_model)
+
+
 if __name__ == '__main__':
     pass
 # ========================= EOF ====================================================================
