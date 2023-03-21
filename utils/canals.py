b6e8e04af24257d916c0c973cdea9beb8d61e88e
dgketchum <dgketchum@gmail.com>
2023-03-21 12:03:03 -0600

    canal data extraction for Great Plains and Upper Colorado, convert to meter cube

--- utils/canals.py
+++ utils/canals.py
@@ -1,11 +1,14 @@
 import os
-from pprint import pprint
+from calendar import monthrange
+import json
 
 import requests
 import numpy as np
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
+
+replace = '!@#$%^&*()[]{};:,./<>?\|`~-=_+'
 
 
 def find_canals_ucrb(file_path, out_path):
@@ -43,74 +46,111 @@ def find_canals_gp(file_path, out_path):
     df.to_file(out_path, crs='EPSG:4326')
 
 
-def get_data_ucrb():
-    dtype = {'19': 'cfs', '20': 'af', '1191': 'cfs'}
-    ex = 'https://www.usbr.gov/uc/water/hydrodata/gage_data/451/json/19.json'
-
-
 def get_data_gp(shp, out_dir):
     sdf = gpd.read_file(shp)
+    # incoming data are in acre-foot per month
     dtr = pd.date_range('1987-01-01', '2021-12-31', freq='M')
     for i, r in sdf.iterrows():
+        converted = False
 
         if r['StationSta'] not in ['WY', 'MT']:
             continue
 
         df = pd.DataFrame(columns=['q'], index=dtr)
-        sid = r['StationID']
-        # if sid != 'SFKDIVCO':
-        #     continue
-        print('\n\n{} {}'.format(r['StationNam'], sid))
-        txt = requests.get('https://www.usbr.gov/gp-bin/inventory.pl?site={}'.format(sid)).content.decode('utf-8')
+        sid, _name = r['StationID'], r['StationNam'].replace('/', '_')
 
+        print('\n\n{} {}'.format(_name, sid))
+        res070 = 'https://www.usbr.gov/gp-bin/res070.pl?station={}&parameters=AF.QJ&byear=1987&eyear=2021'.format(sid)
+        txt = requests.get(res070).content.decode('utf-8')
+        txt = txt.replace('-----', '0')
+        splt = [t.split() for t in txt.splitlines()]
         try:
-            splt = [t.split() for t in txt.splitlines() if 'QC' in t][0]
+            cols = splt[10][:13]
         except IndexError:
-            splt = [t.split() for t in txt.splitlines() if 'Q' in t][0]
-            pprint([t.split() for t in txt.splitlines() if 'Q' in t])
+            print(splt[2])
+            continue
+        data = [x[:13] for x in splt[14:]]
+        for i, x in enumerate(data):
+            try:
+                _ = int(x[0])
+            except ValueError:
+                break
+        data = data[:i]
+        c = pd.DataFrame(data=data, columns=cols)
+        c.index = c['Year']
+        c.drop(columns=['Year'], inplace=True)
+        c.columns = list(range(10, 13)) + list(range(1, 10))
+
+        for i, r in c.iterrows():
+            for m, v in r.items():
+                y = int(i)
+                if m > 9:
+                    y -= 1
+                end_mo = monthrange(int(y), m)[1]
+                # convert to meter cube
+                df.loc['{}-{}-{}'.format(i, m, end_mo), 'q'] = float(v) * 1233.48
+                converted = True
+
+        july = df.loc[[i for i in df.index if i.month == 7], 'q']
+        july = july[july > 0]
+        print(
+            'july mean: {:.1f} {} at {} {}, {} years'.format(july.mean() / 1e6, 'm^3 * 1e6', sid, _name, july.shape[0]))
+
+        df['q'] = df['q'].values.astype(float)
+        print('{} of {} at {}'.format(np.count_nonzero(~np.isnan(df['q'].values)), df.shape[0], _name))
+        assert converted
+        ofile = os.path.join(out_dir, '{}_{}.csv'.format(sid, _name.replace(replace, '_')))
+        df.to_csv(ofile)
+
+
+def get_data_ucrb(shp, out_dir, unit_strict=True):
+    sdf = gpd.read_file(shp)
+    sdf = sdf.sort_values('SID')
+    dtr = pd.date_range('1987-01-01', '2021-12-31', freq='M')
+    converted = False
+    for i, r in sdf.iterrows():
+
+        sid, _name, dtype = r['SID'], r['site_name'], r['DTYPE']
+        units = r['unit_commo']
+
+        if units != 'cfs' and unit_strict:
             continue
 
-        units = splt[-2]
-        yr_splt = splt[-1].split('-')
-        syr, eyr = int(yr_splt[0]), int(yr_splt[1])
-        pprint([t.split() for t in txt.splitlines() if 'Q' in t])
-        print('==== Using QC: {} - {} in {} ====='.format(syr, eyr, units))
+        df = pd.DataFrame(columns=['q'], index=dtr)
+        print('\n\n{} {}'.format(_name, sid))
+        url = 'https://www.usbr.gov/uc/water/hydrodata/gage_data/{}/json/{}.json'.format(sid, dtype)
+        txt = requests.get(url).content.decode('utf-8')
 
-        for year in range(syr, eyr + 1):
+        try:
+            js = json.loads(txt)
+            c = pd.DataFrame(columns=js['columns'], data=js['data'])
+        except Exception as e:
+            print(e, sid, _name, units)
+            continue
 
-            try:
-                url = 'https://www.usbr.gov/gp-bin/webdaycsv.pl?parameter={}%20Q&syer={}' \
-                      '&smnth=1&sdy=1&eyer={}&emnth=12&edy=31&format=2'.format(sid, year, year)
+        c.index = [pd.to_datetime(x) for x in c['datetime']]
+        col = js['columns'][1]
+        c.drop(columns=['datetime'], inplace=True)
 
-                c = pd.read_csv(url, skiprows=20, infer_datetime_format=True, parse_dates=True)
+        if units == 'acre-feet':
+            c = c.resample('M').agg(pd.DataFrame.sum, skipna=False)
+        elif units == 'cfs':
+            # cfs to m ^3 d ^-1
+            c = c * 2446.58
+            c = c.resample('M').agg(pd.DataFrame.sum, skipna=False)
+            converted = True
+        else:
+            raise NotImplementedError('Unknown units and resampling technique')
 
-                c.columns = ['date', 'q']
-                end_ = [i for i, x in enumerate(c['date']) if 'END' in x][0]
-                c = c.iloc[:end_]
+        july = c.loc[[i for i in c.index if i.month == 7], col]
+        july = july[july > 0]
+        print('july mean: {:.1f} {} at {} {}, {} years'.format(july.mean(), 'm^3', sid, _name, july.shape[0]))
 
-                if c.empty:
-                    print('{} empty, skipping'.format(year))
-                    continue
-
-                c.index = [pd.to_datetime(x) for x in c['date']]
-                c = c[['q']]
-                c[c['q'] == 998877] = np.nan
-                c = c.resample('D').agg(pd.DataFrame.mean, skipna=False)
-
-                if len(c.loc['{}-05-01'.format(year): '{}-9-30'.format(year)]) < 153:
-                    print('{} incomplete, skipping'.format(year))
-                    continue
-
-                # convert to cubic meter per day
-                c = c * 60 * 60 * 24 * 0.02831
-                c = c.resample('M').agg(pd.DataFrame.sum, skipna=False)
-                df.loc[c.index, 'q'] = c['q'].values
-
-            except Exception as e:
-                print(e, year, sid)
-
-        ofile = os.path.join(out_dir, '{}.csv'.format(sid))
-        df.to_csv(ofile)
+        ind_match = [i for i in c.index if i in df.index]
+        df.loc[ind_match, 'q'] = c.loc[ind_match, col]
+        assert converted
+        ofile = os.path.join(out_dir, '{}_{}.csv'.format(sid, _name.replace(replace, '_')))
+        df.to_csv(ofile, float_format='%.3f')
 
 
 if __name__ == '__main__':
@@ -128,5 +168,5 @@ if __name__ == '__main__':
 
     od = '/media/research/IrrigationGIS/impacts/canals/hydrographs'
     get_data_gp(gp_out, od)
-
+    get_data_ucrb(ucrb_out, od, unit_strict=True)
 # ========================= EOF ====================================================================
